7b07a241ca2ac3dc77340a5538017b7fa570ce33
Kiley Lewis <kileylewis32@gmail.com>
2019-05-27 20:32:04 -0500

    lodash

--- lodash.js
+++ lodash.js
@@ -1,9 +1,41 @@
 var faker = require('faker');
 
-  var name = faker.name.findName();
-  var randomEmail = faker.internet.email();
-  
 var chunk = require('lodash.chunk');
 var reverse = require('lodash.reverse');
 var without = require('lodash.without');
 var shuffle = require('lodash.shuffle');
+
+let personOne = faker.name.findName();
+let personTwo = faker.name.findName();
+let personThree = faker.name.findName();
+let personFour = faker.name.findName();
+
+
+
+let myArr = [];
+
+myArr.push(personOne, personTwo, personThree, personFour);
+
+console.log("ARRAY OF DATA: ", myArr);
+
+//shuffle
+
+let shuffleArr = function(){
+    myShuffleArr = shuffle(myArr);
+  };
+
+  shuffleArr();
+
+console.log("ARRAY SHUFFLE: ", myShuffleArr);
+
+//chunk 
+
+console.log("ARRAY CHUNK: ", chunk(myArr, 2));
+
+//reverse
+
+console.log("ARRAY REVERSE: ", reverse(myArr));
+
+//without
+
+console.log("ARRAY WITHOUT: ", without(myArr, personOne));
